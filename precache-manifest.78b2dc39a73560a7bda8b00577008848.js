dae846175c72c8b0d3880c574d0a3f1318b2e042
Nick Mattise <n.mattise@gmail.com>
2019-08-19 19:37:52 -0400

    Updates

---
+++ precache-manifest.78b2dc39a73560a7bda8b00577008848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11a1c6bee5f9bd5c9d27cf0632b8bcc0",
+    "url": "/static/media/Flyer.11a1c6be.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "34618a1e5af702de89d7",
+    "url": "/static/js/main.1555c3ca.chunk.js"
+  },
+  {
+    "revision": "252c1d37f4320f6842bb",
+    "url": "/static/js/2.276d0784.chunk.js"
+  },
+  {
+    "revision": "34618a1e5af702de89d7",
+    "url": "/static/css/main.19545a43.chunk.css"
+  },
+  {
+    "revision": "3a2fbeb493543255f1a43e40bf17261c",
+    "url": "/index.html"
+  }
+];
